34232f85daf7533a048e399a2ce49b404575f3f0
Muhammad <muhamad.alyasaev@gmail.com>
2023-11-23 23:19:03 +0300

    Для брата Заура из Инста

--- src/redux/Selectors/filterSelector.js
+++ src/redux/Selectors/filterSelector.js
@@ -1,104 +1,108 @@
 
 // selectors.js
-// export const getFilteredProducts = (state) => {
-//     const { products, sideBar } = state.prCatalogPage;
-//     const activeConsistsCh  = sideBar.present_consists.filter(ch=> ch.isActive)
-//     const activePurposesCh  = sideBar.purposes.filter(ch=> ch.isActive)
-//     const activePackagesCh  = sideBar.packages.filter(ch=> ch.isActive)
-//
-//     let activeCatIds = sideBar.categories.filter(category => category.isActive).map(category => category.id);
-//     // если будет выбрана категория с id=1 "Все подарки" то возвращаем все товары
-//     if (activeCatIds[0] === 1){
-//         const commonProducts = [];
-//         debugger;
-//         if (activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 1){
-//             commonProducts.push(...products.filter(p=>p.isEdible))
-//         } else if(activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 2){
-//             commonProducts.push(...products.filter(p=>p.isEdible===false))
-//         }
-//         if (activePackagesCh.length !== 0 && activePackagesCh[0].id === 1){
-//             commonProducts.push(...products.filter(p=>p.packing_id===1))
-//         } else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 2){
-//             commonProducts.push(...products.filter(p=>p.packing_id===2))
-//         }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 3){
-//             commonProducts.push(...products.filter(p=>p.packing_id===3))
-//         }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 4){
-//             commonProducts.push(...products.filter(p=>p.packing_id===4))
-//         }
-//         if (activePurposesCh.length !== 0 && activePurposesCh[0].id === 1){
-//             commonProducts.push(...products.filter(p=>p.purpose_id===1))
-//         } else if(activePurposesCh.length !== 0 && activePurposesCh[0].id === 2){
-//             commonProducts.push(...products.filter(p=>p.purpose_id===2))
-//         }else if(activePurposesCh.length !== 0 && activePurposesCh[0].id === 3){
-//             commonProducts.push(...products.filter(p=>p.purpose_id===3))
-//         }else if(activePurposesCh.length !== 0 && activePurposesCh[0].id === 4){
-//             commonProducts.push(...products.filter(p=>p.purpose_id===4))
-//         }
-//         if(commonProducts.length!==0){
-//             return commonProducts
-//         } else {
-//             return products
-//         }
-//     }
-//
-//     const pr_c = products.filter(product => {
-//         // Возвращаем true, если категория продукта входит в массив активных категорий
-//         return activeCatIds.includes(product.category_id);
-//     });
-//     if (activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 1){
-//         return pr_c.filter(p=>p.isEdible)
-//     } else if(activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 2){
-//         return pr_c.filter(p=>p.isEdible===false)
-//     }
-//     if (activePackagesCh.length !== 0 && activePackagesCh[0].id === 1){
-//         return pr_c.filter(p=>p.packing_id===1)
-//     } else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 2){
-//         return pr_c.filter(p=>p.packing_id===2)
-//     }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 3){
-//         return pr_c.filter(p=>p.packing_id===3)
-//     }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 4){
-//         return pr_c.filter(p=>p.packing_id===4)
-//     }
-//
-//     if (pr_c.length === 0){return []} else {return pr_c}
-// };
 
+// -------------------мой код
 export const getFilteredProducts = (state) => {
     const { products, sideBar } = state.prCatalogPage;
+    const activeConsistsCh  = sideBar.present_consists.filter(ch=> ch.isActive)
+    const activePurposesCh  = sideBar.purposes.filter(ch=> ch.isActive)
+    const activePackagesCh  = sideBar.packages.filter(ch=> ch.isActive)
 
-    const activeConsistsCh = sideBar.present_consists.filter(ch => ch.isActive);
-    const activePurposesCh = sideBar.purposes.filter(ch => ch.isActive);
-    const activePackagesCh = sideBar.packages.filter(ch => ch.isActive);
-
-    const activeCatIds = sideBar.categories.filter(category => category.isActive).map(category => category.id);
-
-    return products.filter(product => {
-        // Фильтрация по категории
-        if (activeCatIds.length > 0 && !activeCatIds.includes(product.category_id)) {
-            return false;
+    let activeCatIds = sideBar.categories.filter(category => category.isActive).map(category => category.id);
+    // если будет выбрана категория с id=1 "Все подарки" то возвращаем все товары
+    if (activeCatIds[0] === 1){
+        const commonProducts = [];
+        debugger;
+        if (activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 1){
+            commonProducts.push(...products.filter(p=>p.isEdible))
+        } else if(activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 2){
+            commonProducts.push(...products.filter(p=>p.isEdible===false))
         }
-
-        // Фильтрация по составу
-        if (activeConsistsCh.length > 0 && !activeConsistsCh.some(ch => ch.id === (product.isEdible ? 1 : 2))) {
-            return false;
+        if (activePackagesCh.length !== 0 && activePackagesCh[0].id === 1){
+            commonProducts.push(...products.filter(p=>p.packing_id===1))
+        } else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 2){
+            commonProducts.push(...products.filter(p=>p.packing_id===2))
+        }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 3){
+            commonProducts.push(...products.filter(p=>p.packing_id===3))
+        }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 4){
+            commonProducts.push(...products.filter(p=>p.packing_id===4))
         }
-
-        // Фильтрация по упаковке
-        if (activePackagesCh.length > 0 && !activePackagesCh.some(ch => ch.id === product.packing_id)) {
-            return false;
+        if (activePurposesCh.length !== 0 && activePurposesCh[0].id === 1){
+            commonProducts.push(...products.filter(p=>p.purpose_id===1))
+        } else if(activePurposesCh.length !== 0 && activePurposesCh[0].id === 2){
+            commonProducts.push(...products.filter(p=>p.purpose_id===2))
+        }else if(activePurposesCh.length !== 0 && activePurposesCh[0].id === 3){
+            commonProducts.push(...products.filter(p=>p.purpose_id===3))
+        }else if(activePurposesCh.length !== 0 && activePurposesCh[0].id === 4){
+            commonProducts.push(...products.filter(p=>p.purpose_id===4))
         }
-
-        // Фильтрация по назначению
-        if (activePurposesCh.length > 0 && !activePurposesCh.some(ch => ch.id === product.purpose_id)) {
-            return false;
+        if(commonProducts.length!==0){
+            return commonProducts
+        } else {
+            return products
         }
+    }
 
-        return true;
+    const pr_c = products.filter(product => {
+        // Возвращаем true, если категория продукта входит в массив активных категорий
+        return activeCatIds.includes(product.category_id);
     });
+    if (activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 1){
+        return pr_c.filter(p=>p.isEdible)
+    } else if(activeConsistsCh.length !== 0 && activeConsistsCh[0].id === 2){
+        return pr_c.filter(p=>p.isEdible===false)
+    }
+    if (activePackagesCh.length !== 0 && activePackagesCh[0].id === 1){
+        return pr_c.filter(p=>p.packing_id===1)
+    } else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 2){
+        return pr_c.filter(p=>p.packing_id===2)
+    }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 3){
+        return pr_c.filter(p=>p.packing_id===3)
+    }else if(activePackagesCh.length !== 0 && activePackagesCh[0].id === 4){
+        return pr_c.filter(p=>p.packing_id===4)
+    }
+
+    if (pr_c.length === 0){return []} else {return pr_c}
 };
 
 
 
+
+
+// ---------------------------------------------------------Код чат-jpt
+// export const getFilteredProducts = (state) => {
+//     const { products, sideBar } = state.prCatalogPage;
+//
+//     const activeConsistsCh = sideBar.present_consists.filter(ch => ch.isActive);
+//     const activePurposesCh = sideBar.purposes.filter(ch => ch.isActive);
+//     const activePackagesCh = sideBar.packages.filter(ch => ch.isActive);
+//
+//     const activeCatIds = sideBar.categories.filter(category => category.isActive).map(category => category.id);
+//
+//     return products.filter(product => {
+//         // Фильтрация по категории
+//         if (activeCatIds.length > 0 && !activeCatIds.includes(product.category_id)) {
+//             return false;
+//         }
+//
+//         // Фильтрация по составу
+//         if (activeConsistsCh.length > 0 && !activeConsistsCh.some(ch => ch.id === (product.isEdible ? 1 : 2))) {
+//             return false;
+//         }
+//
+//         // Фильтрация по упаковке
+//         if (activePackagesCh.length > 0 && !activePackagesCh.some(ch => ch.id === product.packing_id)) {
+//             return false;
+//         }
+//
+//         // Фильтрация по назначению
+//         if (activePurposesCh.length > 0 && !activePurposesCh.some(ch => ch.id === product.purpose_id)) {
+//             return false;
+//         }
+//
+//         return true;
+//     });
+// };
 
 
 // return products.filter((product) => {
